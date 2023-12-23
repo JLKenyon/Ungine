27de3e4cb4c523a504eeb78c25998f876816d6c9
John Kenyon <jlkenyon@gmail.com>
2023-12-22 23:52:07 -0800

    Exmaple: add fake blue player and red enemies

--- UngineScrollingShooter/src/main.js
+++ UngineScrollingShooter/src/main.js
@@ -4,6 +4,14 @@ let app = createApp({
     template: `<h1>My shooter game!</h1>
     <svg width="600" height="600" viewbox="0 0 600 600">
       <rect x="0" y="0" width="600" height="600" style="fill:#000" />
+
+      <rect x="300" y="550" width="20" height="30" style="fill:#00f" />
+
+      <rect x="100" y="150" width="30" height="20" style="fill:#f00" />
+      <rect x="200" y="150" width="30" height="20" style="fill:#f00" />
+      <rect x="300" y="150" width="30" height="20" style="fill:#f00" />
+      <rect x="400" y="150" width="30" height="20" style="fill:#f00" />
+      <rect x="500" y="150" width="30" height="20" style="fill:#f00" />
     </svg>`
 });
 
