4d95a5bef046dbefb0a2d40e00e1106c1f4b62a8
John Kenyon <jlkenyon@gmail.com>
2023-12-22 23:47:19 -0800

    Add an SVG element

--- UngineScrollingShooter/src/main.js
+++ UngineScrollingShooter/src/main.js
@@ -1,7 +1,10 @@
 import {createApp} from 'vue';
 
 let app = createApp({
-    template: `<h1>Hello World!</h1>`,
+    template: `<h1>My shooter game!</h1>
+    <svg width="600" height="600" viewbox="0 0 600 600">
+      <rect x="0" y="0" width="600" height="600" style="fill:#000" />
+    </svg>`
 });
 
 function main() {
