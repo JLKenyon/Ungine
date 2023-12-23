a89869325f1c041eef6f8f33e3aaaac70f286354
John Kenyon <jlkenyon@gmail.com>
2023-12-22 23:14:11 -0800

    Add simple Vue stub

---
+++ UngineScrollingShooter/README.md
@@ -0,0 +1,6 @@
+# Commands to get up and running
+
+ npm install
+ webpack-dev-server --open
+
+A web browser will auto open. You can also open your preferred browser to localhost:8000
--- UngineScrollingShooter/src/main.js
+++ UngineScrollingShooter/src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import {createApp} from 'vue';
+
+let app = createApp({
+    template: `<h1>Hello World!</h1>`,
+});
+
+function main() {
+    app.mount('#app');
+}
+
+document.addEventListener('DOMContentLoaded', main);
